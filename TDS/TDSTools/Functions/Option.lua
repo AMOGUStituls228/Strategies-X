c786581b3cefa2c8deff5e04e4232755f3e6605b
AMOGUStituls228 <damogus18@gmail.com>
2025-08-21 23:42:43 +0700

    Update Option.lua

--- TDS/TDSTools/Functions/Option.lua
+++ TDS/TDSTools/Functions/Option.lua
@@ -1,50 +1,42 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local RemoteFunction = if not GameSpoof then ReplicatedStorage:WaitForChild("RemoteFunction") else SpoofEvent
 local RemoteEvent = if not GameSpoof then ReplicatedStorage:WaitForChild("RemoteEvent") else SpoofEvent
 --[[{
-    ["TowerIndex"] = "",
-    ["TypeIndex"] = "",
-    ["Name"] = "",
-    ["Value"] = "",
     ["Wave"] = number,
     ["Minute"] = number,
     ["Second"] = number,
 }]]
 return function(self, p1)
-    local tableinfo = p1
-    local Tower = tableinfo["TowerIndex"]
-    local OptName = tableinfo["Name"]
-    local OptValue = tableinfo["Value"]
+    local tableinfo = p1--ParametersPatch("Skip",...)
     local Wave,Min,Sec,InWave = tableinfo["Wave"] or 0, tableinfo["Minute"] or 0, tableinfo["Second"] or 0, tableinfo["InBetween"] or false
     if not CheckPlace() then
         return
     end
-    SetActionInfo("Option","Total")
+    local VoteGUI = LocalPlayer.PlayerGui:WaitForChild("ReactOverridesVote"):WaitForChild("Frame"):WaitForChild("votes"):WaitForChild("vote") -- it is what it is
+    SetActionInfo("Skip","Total")
     task.spawn(function()
         if not TimeWaveWait(Wave, Min, Sec, InWave, tableinfo["Debug"]) then
             return
         end
-        local OptionCheck
-        local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
-        task.spawn(function()
-            task.wait(2)
-            while not (type(OptionCheck) == "boolean" and OptionCheck) do
-                ConsoleWarn(`Cannot Set Option (Name: {OptName}, Value: {OptValue}) On Tower Index: {Tower}, Type: \"{TowerType}\", (Wave {Wave}, Min: {Min}, Sec: {Sec}, InBetween: {InWave})`)
-                task.wait(1)
-            end
-        end)
+        local SkipCheck
+        if VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required` then
+            repeat
+                task.wait()
+            until VoteGUI:WaitForChild("count").Text == `0/{#Players:GetChildren()} Required`
+        end
+        if VoteGUI.Position ~= UDim2.new(0.5, 0, 0.5, 0) then --UDim2.new(scale_x, offset_x, scale_y, offset_y)
+            return
+        end
         repeat
-            if not TowersCheckHandler(Tower) then
+            if VoteGUI:WaitForChild("prompt").Text ~= "Skip Wave?" then
                 return
             end
-            OptionCheck = RemoteFunction:InvokeServer("Troops","Option","Set",{
-                ["Troop"] = TowersContained[Tower].Instance, 
-                ["Name"] = OptName,
-                ["Value"] = OptValue,
-            })
+            SkipCheck = RemoteFunction:InvokeServer("Voting", "Skip")
             task.wait()
-        until type(OptionCheck) == "boolean" and OptionCheck
-        SetActionInfo("Option")
-        ConsoleInfo(`Set Option (Name: {OptName}, Value: {OptValue}) On Tower Index: {Tower}, Type: \"{TowerType}\", (Wave {Wave}, Min: {Min}, Sec: {Sec}, InBetween: {InWave})`)
+        until SkipCheck or VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required`
+        SetActionInfo("Skip")
+        ConsoleInfo(`Skipped Wave {Wave} (Min: {Min}, Sec: {Sec}, InBetween: {InWave})`)
     end)
-end
+        end
