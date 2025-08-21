27eb06a2999692e6559684daf91a4f31a8076571
AMOGUStituls228 <damogus18@gmail.com>
2025-08-21 23:44:24 +0700

    Update Sell.lua

--- TDS/TDSTools/Functions/Sell.lua
+++ TDS/TDSTools/Functions/Sell.lua
@@ -4,40 +4,47 @@ local RemoteEvent = if not GameSpoof then ReplicatedStorage:WaitForChild("Remote
 --[[{
     ["TowerIndex"] = "",
     ["TypeIndex"] = "",
+    ["Name"] = "",
+    ["Value"] = "",
     ["Wave"] = number,
     ["Minute"] = number,
     ["Second"] = number,
 }]]
 return function(self, p1)
-    local tableinfo = p1--ParametersPatch("Sell",...)
+    local tableinfo = p1
     local Tower = tableinfo["TowerIndex"]
-    local Wave,Min,Sec,InWave = tableinfo["Wave"] or 0, tableinfo["Minute"] or 0, tableinfo["Second"] or 0, tableinfo["InBetween"] or false 
+    local OptName = tableinfo["Name"]
+    local OptValue = tableinfo["Value"]
+    local Wave,Min,Sec,InWave = tableinfo["Wave"] or 0, tableinfo["Minute"] or 0, tableinfo["Second"] or 0, tableinfo["InBetween"] or false
     if not CheckPlace() then
         return
     end
-    for i = 1, #Tower do
-        SetActionInfo("Sell","Total")
-    end
+    SetActionInfo("Option","Total")
     task.spawn(function()
         if not TimeWaveWait(Wave, Min, Sec, InWave, tableinfo["Debug"]) then
             return
         end
-        for i,v in next, Tower do
-            task.spawn(function()
-                local SoldCheck
-                repeat
-                    if not TowersCheckHandler(v) then
-                        return
-                    end
-                    SoldCheck = RemoteFunction:InvokeServer("Troops","Sell",{
-                        ["Troop"] = TowersContained[v].Instance
-                    })
-                    task.wait()
-                until SoldCheck or not TowersContained[v].Instance:FindFirstChild("HumanoidRootPart")
-                local TowerType = GetTypeIndex(tableinfo["TypeIndex"],v)
-                SetActionInfo("Sell")
-                ConsoleInfo((not SoldCheck and "Already " or "").."Sold Tower Index: "..v..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..")")
-            end)
-        end
+        local OptionCheck
+        local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
+        task.spawn(function()
+            task.wait(2)
+            while not (type(OptionCheck) == "boolean" and OptionCheck) do
+                ConsoleWarn(`Cannot Set Option (Name: {OptName}, Value: {OptValue}) On Tower Index: {Tower}, Type: \"{TowerType}\", (Wave {Wave}, Min: {Min}, Sec: {Sec}, InBetween: {InWave})`)
+                task.wait(1)
+            end
+        end)
+        repeat
+            if not TowersCheckHandler(Tower) then
+                return
+            end
+            OptionCheck = RemoteFunction:InvokeServer("Troops","Option","Set",{
+                ["Troop"] = TowersContained[Tower].Instance, 
+                ["Name"] = OptName,
+                ["Value"] = OptValue,
+            })
+            task.wait()
+        until type(OptionCheck) == "boolean" and OptionCheck
+        SetActionInfo("Option")
+        ConsoleInfo(`Set Option (Name: {OptName}, Value: {OptValue}) On Tower Index: {Tower}, Type: \"{TowerType}\", (Wave {Wave}, Min: {Min}, Sec: {Sec}, InBetween: {InWave})`)
     end)
-end
+                end
